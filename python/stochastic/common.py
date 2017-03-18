9cc995b5b7d79ca7927b625cb3fab6b2984a6b3a
Chad Estioco <chadestioco@gmail.com>
2017-03-19 05:09:50 +0800

    filthy tabs.

--- python/stochastic/common.py
+++ python/stochastic/common.py
@@ -9,9 +9,9 @@ used in Chapter 2.
 """
 
 def euc_2d(p1, p2):
-	dx = p1[0] - p2[0]
-	dy = p1[1] - p2[1]
-	return math.sqrt((dx ** 2) + (dy ** 2))
+    dx = p1[0] - p2[0]
+    dy = p1[1] - p2[1]
+    return math.sqrt((dx ** 2) + (dy ** 2))
 
 def random_permutation(cities):
     """
